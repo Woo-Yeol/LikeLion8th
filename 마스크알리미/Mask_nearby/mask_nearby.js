2b7d7e9b9c5603ebd5ff0be5501ae5035cf6af5c
Woo-Yeol <kkkdnduf@likelion.org>
2020-08-05 23:18:05 +0900

    Search

--- 마스크알리미/Mask_nearby/mask_nearby.js
+++ 마스크알리미/Mask_nearby/mask_nearby.js
@@ -5,12 +5,41 @@ var options = { //지도를 생성할 때 필요한 기본 옵션
 };
 
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+var ps = new kakao.maps.services.Places(); 
+
 
 // 버튼 누르거나 Enter 눌렀을 때 검색이 되도록 만들기
 let search_btn = document.querySelector(".search-btn");
 let search_bar = document.querySelector("#search-bar");
-
 console.log(search_bar);
 console.log(search_btn);
 
-search
+search_btn.addEventListener("click", () => {
+    let keyword = search_bar.value;
+    if(keyword) {
+    console.log(keyword + " 검색하셨습니다.");
+    keywordSearch(keyword);
+    } 
+    else { 
+        alert("검색어를 입력해주세요");
+    }
+});
+
+search_bar.addEventListener("keyup", () => {
+    //keycode 13 = Enter Key
+    if(event.keyCode === 13){
+        search_btn.click();
+    }
+});
+
+function keywordSearch(keyword){
+    ps.keywordSearch(keyword , keywordSearchCallback);
+}
+
+// 키워드 검색 완료 시 호출되는 콜백함수 입니다
+function keywordSearchCallback (data, status, pagination) {
+    if (status === kakao.maps.services.Status.OK) {
+        let center = new kakao.maps.LatLng(data[0].y, data[0].x); 
+        map.setCenter(center);
+    } 
+}
